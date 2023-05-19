811719e937b370f616ab991be59f3825033e253a
samamasikander <134098937+samamasikander@users.noreply.github.com>
2023-05-20 00:56:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+
+var a=5;
+var b=15;
+var c = a+b;
+alert(c);
+var a="ali";
+var b=15;
+var c = a+b;
+alert(c);
+var a="6 this is concatenation";
+var b=15;
+var c = a+b;
+alert(c);
+var a=5;
+var b="4";
+var c = a*b;
+alert(c);
+var a = 3;
+var b = 5;
+var c = 7;
+var d = b-a*c;
+alert(d);
+var a = 3;
+var b = 5;
+var c = 7;
+var d = (b-a)*c;
+alert(d);
+//post decrement
+var a = 5 ;
+var b = a++ ;
+alert(b);
+//post increment
+var a = 3;
+var b = ++a;
+alert(b);
+alert(a);
+//example of this
+var x = 10;
+var y = x++ + ++x - x + x++ - ++x + x;
+alert(y);
+var a = "your species";
+var b = "human";
+var c = prompt(a,b);
